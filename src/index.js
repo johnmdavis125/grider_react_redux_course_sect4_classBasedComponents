6da98147786e7e378aa70e862b95b4830e231895
John Davis <johnmdavis125@gmail.com>
2021-02-25 17:12:18 -0500

    refactored dataloading from constructor function to componentDidMount lifecycle method

--- src/index.js
+++ src/index.js
@@ -15,17 +15,24 @@ class App extends React.Component {
 
         // this is the only time we do direct assignment to this.state (i.e.in constr)
         this.state = { lat: null, errorMessage: '' };   
+    }
 
+    componentDidMount() {
         window.navigator.geolocation.getCurrentPosition(
-            position => {
-                // we called 'setState' to update the state of the component!
-                this.setState({ lat: position.coords.latitude })
-            },
-            (err) => {
-                this.setState({ errorMessage: err.message })
-            }
+            position => this.setState({ lat: position.coords.latitude }),
+            err => this.setState({ errorMessage: err.message })
         );
-}
+    }
+
+
+    // lifecycle methods
+    // componentDidMount() {
+    //     console.log('my component was rendered to the screen'); 
+    // }
+    // componentDidUpdate() {
+    //     console.log('my component was just updated. it re-rendered!'); 
+    // }
+
     render() {
         if (this.state.errorMessage && !this.state.lat) {
             return <div>Error: {this.state.errorMessage}</div>
